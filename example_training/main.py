60538da8c0ce25e5d24f1f193a4e405fa4c951a6
Mathias Lundteigen Mohus <mathialm@ntnu.no>
2022-03-17 16:14:53 +0100

    Updated to work

--- example_training/main.py
+++ example_training/main.py
@@ -1,7 +1,8 @@
 if __name__ == "__main__":
-    from gan_task import GANTask
-    from config import config
-    from gpu_task_scheduler.gpu_task_scheduler import GPUTaskScheduler
-    
-    scheduler = GPUTaskScheduler(config=config, gpu_task_class=GANTask)
-    scheduler.start()
+	from gan_task import GANTask
+	from config import config
+	from gpu_task_scheduler.gpu_task_scheduler import GPUTaskScheduler
+	print("Started working")
+	scheduler = GPUTaskScheduler(config=config, gpu_task_class=GANTask)
+	print("Has set up GPUTaskScheduler")
+	scheduler.start()
