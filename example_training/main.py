a12bbe126553074c283cade235b88514dc1c5d1a
Mathias Lundteigen Mohus <mathialm@ntnu.no>
2022-03-21 16:06:35 +0100

    Results file location

--- example_training/main.py
+++ example_training/main.py
@@ -2,6 +2,8 @@ if __name__ == "__main__":
 	from gan_task import GANTask
 	from config import config
 	from gpu_task_scheduler.gpu_task_scheduler import GPUTaskScheduler
+	import tensorflow as tf
+	print("Can see tensorflow version "  + str(tf.__version__))
 	print("Started working")
 	scheduler = GPUTaskScheduler(config=config, gpu_task_class=GANTask)
 	print("Has set up GPUTaskScheduler")
